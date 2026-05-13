94a7cb65b3abde1182584d80804c12c718a1fde1
et <phage0307@gmail.com>
2026-05-13 14:30:47 +0800

    ex12

---
+++ ex12/react_login/src/App.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import './App.css';
+import CGU_Login from './cgu_login';
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <div>
+      {CGU_Login()}
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
